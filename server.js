226a53d70e95584a841dad1a9c2cfac49c1c0033
Jesus Arriola <piracarter@hotmail.com>
2018-05-15 13:53:52 -0400

    Se modifica puerto 	modified:   server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000,function(){
+app.listen(8080,function(){
 
-console.log("server is up and running at port 3000");
+console.log("server is up and running at port 8080");
 })
 
 app.get('/', function(request,response){
